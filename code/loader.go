5816c6727b7f7dc6cac728b7f634a45bf74658cb
Venancio <v.romeromo@gmail.com>
2018-11-30 01:27:27 +0100

    fix makefile

--- code/loader.go
+++ code/loader.go
@@ -38,13 +38,13 @@ func main() {
 
 	// Execute the query
 	results, err := db.Query(`SELECT 	orders.orders_id ,
-									   	orders.customers_id,
-									   	orders_products.products_id, 
-									   	CONCAT("/images/",products.products_image) as image_url,
+										orders.customers_id,
+										orders_products.products_id, 
+										CONCAT("/images/",products.products_image) as image_url,
 										CONCAT("https://www.panel555.com/product_info.php?products_id=",orders_products.products_id) as url_product 
-   								FROM
-	 									orders,products,orders_products
-   								WHERE 
+								FROM
+										orders,products,orders_products
+								WHERE 
 										orders.orders_id         = orders_products.orders_id AND
 										products.products_id     = orders_products.products_id AND 
 										products.products_status = 1 AND
